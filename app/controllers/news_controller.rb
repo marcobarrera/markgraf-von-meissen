3096d9d383e009d4841b1c59dda03954ac29e9eb
teresitasachsen <teresitasachsen99@gmail.com>
2021-03-23 14:15:20 -0600

    created actions for each controller

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,2 +1,9 @@
 class NewsController < ApplicationController
+  def index
+    @news = New.all
+  end
+
+  def show
+    @new = New.find(params[:id])
+  end
 end
--- test/controllers/histories_controller_test.rb
+++ test/controllers/histories_controller_test.rb
@@ -4,4 +4,7 @@ class HistoriesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def index
+    @Histories = History.all
+  end
 end
